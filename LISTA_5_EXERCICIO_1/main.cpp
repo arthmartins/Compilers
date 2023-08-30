13a6b5a1628ac3834efb2e00976d471c664341d9
Arthur Martins <arthur.martins@uel.br>
2023-08-30 19:37:54 -0300

    mais algumas coisas feitas

--- LISTA_5_EXERCICIO_1/main.cpp
+++ LISTA_5_EXERCICIO_1/main.cpp
@@ -17,6 +17,68 @@ int transitions[states][alphabet] ={
 };
 bool final_states[states] = {false,false,true,true,true,true,false,false,true};
 
+void lexer(std::string entrada){
+    int last_final = 0;
+    int current_state = 1;
+    int start = 0;
+    int token = 0;
+    int count = 0;
+
+    for(int i = 0 ; i <= entrada.length();i++){
+
+        if(current_state==0 || entrada[i] == '\0'){
+            for(int j = start; j < token;j++){
+                std::cout << entrada[j];
+            }
+            printf("\n");
+            last_final = 0;
+            current_state = 1;
+            
+            start = token;
+            count = start;
+            if(entrada[i] != '\0'){i = start -1;}
+           
+        }
+        else if(entrada[count]< 58 && entrada[count] > 47)
+        {
+            current_state = transitions[current_state][entrada[count]-20];
+            if(final_states[current_state]){ last_final = current_state; token = i+1; }
+            count++;
+
+        }else if(entrada[count]<123  && entrada[count] > 96)
+        {
+            current_state = transitions[current_state][entrada[count]-95];
+            if(final_states[current_state]){ last_final = current_state; token = i+1; }
+            count++;
+
+        }else if(entrada[count]==43)
+        {
+            current_state = transitions[current_state][entrada[count]-43];
+            if(final_states[current_state]){ last_final = current_state; token = i+1; }
+            count++;
+
+        }else if(entrada[count]==45)
+        {
+            current_state = transitions[current_state][entrada[count]-44];
+            if(final_states[current_state]){ last_final = current_state; token = i+1; }
+            count++;
+            
+        }else if (last_final == 0)
+        {
+            std::cout << "ERROR";
+        }
+        
+        
+    }
+    
+    
+    
+     
+
+
+}
+
+
 
 
 int main(){
@@ -26,7 +88,6 @@ int main(){
     while ((c = getchar()) != EOF) {
         entrada += c;
     }
-
-        std::cout << entrada;
-
+    
+    lexer(entrada);
 }
